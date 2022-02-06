a75a272cf68863b71e11eea7c511d6c31aa038d2
Nicu Barbaros <nicholas.barbaros@gmail.com>
2022-02-07 00:29:14 +0200

    👌 First commit of personal folio

---
+++ components/Header.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <div className="nav-bar">
+      <div className="nav-bar-left">
+        <p>nicu barbaros</p>
+        <p>
+          Currently Senior <br /> Software Engineer at Planable
+        </p>
+        <p>
+          Based in <br /> Chisinau Moldova
+        </p>
+      </div>
+      <div className="nav-bar-right">menu</div>
+    </div>
+  );
+}
