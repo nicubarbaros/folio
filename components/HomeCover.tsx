f55e27cb428d09543fad9cbb2325407cbe961d64
Nicu Barbaros <nicholas.barbaros@gmail.com>
2022-02-14 23:01:24 +0200

    👌 Add simple footer

--- components/HomeCover.tsx
+++ components/HomeCover.tsx
@@ -6,16 +6,8 @@ import Reel from './Reel';
 export default function HomeCover() {
   return (
     <div className="home-cover">
-      {/* <h1 className="big-bold-font">Nicu Barbaros</h1> */}
-
-      {/* <div className="image">
-        <Image src={'/images/home.jpeg'} objectFit="cover" layout="fill" /> */}
-      {/* <Image src={"/images/profile.jpeg"} objectFit="cover" layout="f	ill" /> */}
-      {/* <Image src={"/images/home2.jpeg"} objectFit="cover" layout="fill" /> */}
-      {/* </div> */}
       <AnimatedTitle text="Creative" />
       <AnimatedTitle text="Developer" />
-      {/* <h1 className="big-bold-font">I'm Nicu</h1> */}
 
       {/* <Reel src={"https://player.vimeo.com/video/477735526?background=1"} /> */}
     </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -8,6 +8,7 @@ import HomeCover from "../components/HomeCover";
 import CustomCursor from "../components/CustomCursor";
 import CursorManager from "../components/CustomCursor/CursorManager";
 import ScrollIndicator from "../components/CustomCursor/ScrollIndicator";
+import Link from 'next/link';
 
 const Home: NextPage = () => {
   const year = new Date().getFullYear();
@@ -29,14 +30,14 @@ const Home: NextPage = () => {
           <HomeCover />
           <ScrollIndicator />
         </div>
-        <section className="section">
+        <section className="section about-image">
           <div className="image">
             <Image src={'/images/home2.jpeg'} objectFit="cover" layout="fill" />
             {/* <Image src={"/images/profile.jpeg"} objectFit="cover" layout="f	ill" /> */}
             {/* <Image src={"/images/home2.jpeg"} objectFit="cover" layout="fill" /> */}
           </div>
         </section>
-        <section className="section">
+        <section className="section about-text">
           <p className="full-paragraph">
             {/* I’m a moldavian creative front end developer, I create interfaces, help with art direction, and appreciate polished
           motion design pieces. I also bake in free time. */}
@@ -44,6 +45,25 @@ const Home: NextPage = () => {
             growth, and appreciate polished web design pieces. I also bake in free time.
           </p>
         </section>
+
+        <footer className="footer">
+          <h6>Nicu Barbaros</h6>
+
+          <div>
+            <Link href="https://twitter.com/nicubarbaros" passHref>
+              <a target="_blank" rel="noopener noreferrer">
+                Twitter
+              </a>
+            </Link>
+            <Link href="https://twitter.com/nicubarbaros" passHref>
+              <a target="_blank" rel="noopener noreferrer">
+                YouTube
+              </a>
+            </Link>
+          </div>
+
+          <h6>({year}), all rights reserved</h6>
+        </footer>
       </>
     </CursorManager>
   );
