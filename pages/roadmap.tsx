aa27de71b9ea767f30aacc0a1cc7e6dea21037ca
Nicu Barbaros <nicholas.barbaros@gmail.com>
2022-04-13 00:21:22 +0300

    👌 Subscribe to mailchimp using the form from roadmap page

--- pages/roadmap.tsx
+++ pages/roadmap.tsx
@@ -3,12 +3,29 @@ import React, { useState } from 'react';
 export default function Roadmap() {
   const [email, setEmail] = useState('');
 
+  const [message, setMessage] = useState(null);
   const handleChange = event => {
     setEmail(event.target.value);
   };
 
   const handleSubmit = async event => {
     event.preventDefault();
+    const response = await fetch('/api/mailchimp/subscribe', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ email })
+    });
+    const { error } = await response.json();
+    if (error) {
+      // 4. If there was an error, update the message in state.
+      setMessage(error);
+      return;
+    }
+
+    setEmail('');
+    setMessage('Success! 🎉 You are now subscribed to the newsletter.');
   };
   return (
     <div className="roadmap-container">
@@ -23,6 +40,8 @@ export default function Roadmap() {
         <input className="roadmap-input" type="text" value={email} onChange={handleChange} placeholder="Your email" />
         <input type="submit" className="roadmap-button" value="Subscribe" />
       </form>
+
+      {message && <div>{message}</div>}
     </div>
   );
 }
