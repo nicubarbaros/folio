5856b0395820d4c205de09d68ff46b2b4a48872c
Nicu Barbaros <barbarosnicolae93@gmail.com>
2024-04-09 14:55:51 +0300

    👌 Fix errors

--- pages/free-tools.tsx
+++ pages/free-tools.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function FreeTool() {
   return (
     <div>
-      <iframe src="https://free-tool.vercel.app/" width="100%" height="600" frameborder="0"></iframe>
+      <iframe src="https://free-tool.vercel.app/" width="100%" height="600"></iframe>
     </div>
   );
 }
