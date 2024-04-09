58719b9595dcf81fa06cf35be7e4e43d436f4b83
Nicu Barbaros <barbarosnicolae93@gmail.com>
2024-04-09 14:53:56 +0300

    👌 Add free tool

---
+++ pages/free-tools.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function FreeTool() {
+  return (
+    <div>
+      <iframe src="https://free-tool.vercel.app/" width="100%" height="600" frameborder="0"></iframe>
+    </div>
+  );
+}
