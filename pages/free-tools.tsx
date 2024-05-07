89f78d65f501fa0bfc39cbf264128773b5d2e401
Nicu Barbaros <barbarosnicolae93@gmail.com>
2024-05-07 17:45:47 +0300

    👌 New version

--- pages/free-tools.tsx
+++ pages/free-tools.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function FreeTool() {
   return (
     <div>
-      <iframe src="https://93482d1d.planable-free-tools.pages.dev"></iframe>
+      <iframe src="https://d66b8d86.planable-free-tools.pages.dev"></iframe>
     </div>
   );
 }
